63b90705c24523ef89e3e6158aabd4c682b74f38
nick perez <nickperez1285@gmail.com>
2025-12-14 00:00:03 -0800

    2nd

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use("/api/auth", authRoutes);
 
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-// module.exports = app;
+module.exports = app;
